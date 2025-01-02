6cf465d4790c0749a84fceca85840fd760437cf9
ciscol86 <51709658+ciscol86@users.noreply.github.com>
2025-01-02 14:40:38 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,3 +18,15 @@
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# 修改默认IP为 192.168.8.5
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+ 
+# 修改默认主机名
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Ciscol_Router'' package/lean/default-settings/files/zzz-default-settings
+ 
+# 加入编译者信息
+sed -i "s/OpenWrt /Ciscol_Guan build V1.2 @ OpenWrt Lede /g" package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
